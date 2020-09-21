d853cc522eec3bb8ab887a987ea9a5ee26a473f8
Shantovai23 <hasibulhasan23sept@gmail.com>
2020-09-21 20:32:54 +0600

    digital clocl added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Header=()=>{
+    const [secTime,setSecTime]=useState()
+
+    const timeFun=()=>{
+      let  time=new Date().toLocaleTimeString()
+        setSecTime(time)
+    }
+
+    setInterval(timeFun,1000)
+   
     return(<>
        <div className='header'>
-       <h1 >Google Keep</h1>
+       <h1 >Google Keep </h1>
+       <h4>{secTime}</h4>
        </div>
     </>)
 }
